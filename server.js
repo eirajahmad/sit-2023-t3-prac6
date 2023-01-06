079a91287b37244e963e2b06157642b68e1a303f
eirajahmad <46993669+eirajahmad@users.noreply.github.com>
2023-01-07 00:08:32 +1100

    commit
    commit

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use('/api/projects', projectRoutes)
-
 app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
     var firstNumber = parseInt(req.params.firstNumber) 
     var secondNumber = parseInt(req.params.secondNumber)
@@ -20,7 +19,16 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
     }
     else { res.json({result: result, statusCode: 200}).status(200) } 
   })
-
+  app.get('/substractTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber - secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+  })
+ 
 // //mongoDb connection...
 // const MongoClient = require('mongodb').MongoClient; 
 // const uri = 'mongodb+srv://abatool:amna@cluster0.xnvxmni.mongodb.net/?retryWrites=true&w=majority'
--- test/test.js
+++ test/test.js
@@ -24,3 +24,26 @@ describe("Add Two Numbers", function(){
 
     
 });
+describe("Add Two strings", function() {
+    var url = "http://localhost:8080/addTwoNumbers/a/b";
+    it("should return status 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(400);
+            done()
+          });
+    });
+    it("returns the result as null", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.result).to.be.a('null');
+            done()
+          });
+    });
+  });
