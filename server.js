6c83d45c02a73a26456f48d47bd0b392315c32ac
eirajahmad <46993669+eirajahmad@users.noreply.github.com>
2023-01-07 01:18:56 +1100

    commit
    commit

--- server.js
+++ server.js
@@ -1,90 +1,93 @@
-var express = require("express")
-var app = express()
-var cors = require('cors')
-let dbConnect = require("./dbConnect"); 
-let projectRoutes = require("./routes/projectRoutes"); 
-// let projectCollection; 
+var express = require("express");
+var app = express();
+var cors = require("cors");
+let dbConnect = require("./dbConnect");
+let projectRoutes = require("./routes/projectRoutes");
+// let projectCollection;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors())
-app.use('/api/projects', projectRoutes)
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-    var firstNumber = parseInt(req.params.firstNumber) 
-    var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber + secondNumber || null
-    if(result == null) {
-      res.json({result: result, statusCode: 400}).status(400)
-    }
-    else { res.json({result: result, statusCode: 200}).status(200) } 
-  })
-  app.get('/substractTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-    var firstNumber = parseInt(req.params.firstNumber) 
-    var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber - secondNumber || null
-    if(result == null) {
-      res.json({result: result, statusCode: 400}).status(400)
-    }
-    else { res.json({result: result, statusCode: 200}).status(200) } 
-  })
- 
-// //mongoDb connection...
-// const MongoClient = require('mongodb').MongoClient; 
-// const uri = 'mongodb+srv://abatool:amna@cluster0.xnvxmni.mongodb.net/?retryWrites=true&w=majority'
-// const client = new MongoClient(uri, {useNewUrlParser: true})
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(cors());
+app.use("/api/projects", projectRoutes);
+app.get("/addTwoNumbers/:firstNumber/:secondNumber", function(req, res, next) {
+	var firstNumber = parseInt(req.params.firstNumber);
+	var secondNumber = parseInt(req.params.secondNumber);
+	var result = firstNumber + secondNumber || null;
+	if (result == null) {
+		res.json({
+			result: result,
+			statusCode: 400
+		}).status(400);
+	} else {
+		res.json({
+			result: result,
+			statusCode: 200
+		}).status(200);
+	}
+});
+app.get("/substractTwoNumbers/:firstNumber/:secondNumber",
+	function(req, res, next) {
+		var firstNumber = parseInt(req.params.firstNumber);
+		var secondNumber = parseInt(req.params.secondNumber);
+		var result = firstNumber - secondNumber || null;
+		if (result == null) {
+			res.json({
+				result: result,
+				statusCode: 400
+			}).status(400);
+		} else {
+			res.json({
+				result: result,
+				statusCode: 200
+			}).status(200);
+		}
+	}
+);
+app.get("/multiplyTwoNumbers/:firstNumber/:secondNumber",
+	function(req, res, next) {
+		var firstNumber = parseInt(req.params.firstNumber);
+		var secondNumber = parseInt(req.params.secondNumber);
+		var result = firstNumber * secondNumber || null;
+		if (result == null) {
+			res.json({
+				result: result,
+				statusCode: 400
+			}).status(400);
+		} else {
+			res.json({
+				result: result,
+				statusCode: 200
+			}).status(200);
+		}
+	}
+);
+app.get("/divideTwoNumbers/:firstNumber/:secondNumber",
+	function(req, res, next) {
+		var firstNumber = parseInt(req.params.firstNumber);
+		var secondNumber = parseInt(req.params.secondNumber);
+		var result = firstNumber / secondNumber || null;
+		if (result == null) {
+			res.json({
+				result: result,
+				statusCode: 400
+			}).status(400);
+		} else {
+			res.json({
+				result: result,
+				statusCode: 200
+			}).status(200);
+		}
+	}
+);
 
 
-// const createCollection = (collectionName) => {
-//     client.connect((err,db) => {
-//         projectCollection = client.db().collection(collectionName);
-//         if(!err){
-//             console.log('MongoDb Connected')
-//         }
-//         else {
-//             console.log("DB Error: ", err);
-//             process.exit(1);
-//         }
-//     })
-// }
 
-// //insert project....
-// const insertProjects = (project,callback) => {
-//     projectCollection.insert(project,callback);
-// }
-
-// //post api....
-// app.post('/api/projects',(req,res) => {
-//     console.log("New Project added", req.body)
-//     var newProject = req.body;
-//     insertProjects(newProject,(err,result) => {
-//         if(err) {
-//             res.json({statusCode: 400, message: err})
-//         }
-//         else {
-//             res.json({statusCode: 200, message:"Project Successfully added", data: result})
-//         }
-//     })
-// })
-
-// // get project...
-// const getProjects = (callback) => {
-//     projectCollection.find({}).toArray(callback);
-// }
-
-// app.get('/api/projects',(req,res) => {
-//     getProjects((err,result) => {
-//         if(err) {
-//             res.json({statusCode: 400, message: err})
-//         }
-//         else {
-//             res.json({statusCode: 200, message:"Success", data: result})
-//         }
-//     })
-// })
 
 var port = process.env.port || 8080;
-app.listen(port,()=>{
-    console.log("App listening to http://localhost:"+port)
-    //createCollection('Pets')
-})
+app.listen(port, () => {
+	console.log("App listening to http://localhost:" + port);
+	//createCollection('Pets')
+});
--- test/test.js
+++ test/test.js
@@ -1,49 +1,191 @@
 let express = require("express");
 let app = express();
-var expect = require("chai").expect; 
+var expect = require("chai").expect;
 var request = require("request");
 
-describe("Add Two Numbers", function(){
-    var url = 'http://localhost:8080/addTwoNumbers/3/5'; 
-    // first it function....
-    it("returns status 200 to check if api works", function(done){
-        request(url, function(error, response){
-            expect(response.statusCode).to.equal(200);
-            done(); 
-        });
-    });
-
-    // second it function.....
-    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(200);
-            done()
-        });
-    });
-
-    
-});
-describe("Add Two strings", function() {
-    var url = "http://localhost:8080/addTwoNumbers/a/b";
-    it("should return status 200", function(done) {
-        request(url, function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done()
-          });
-    });
-    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(400);
-            done()
-          });
-    });
-    it("returns the result as null", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('null');
-            done()
-          });
+describe("Testing addition", function () {
+  var url = "http://localhost:8080/addTwoNumbers/10/20";
+  // checking status code directly
+  it("statusCode checked 200", function (done) {
+    request(url, function (error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
   });
+
+  // checking status code json body parsed
+  it("body parsed statusCode checked 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // check result is a number
+  it("result returned is a number", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.be.a("number");
+      done();
+    });
+  });
+
+  // check result number is correct
+  it("result of adding 10, 20 is 30", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.equal(30);
+      done();
+    });
+  });
+
+  // check result number is not incorrect
+  it("result is not -100", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.not.equal(-100);
+      done();
+    });
+  });
+});
+
+describe("Testing subtraction", function () {
+  var url = "http://localhost:8080/substractTwoNumbers/10/20";
+  // checking status code directly
+  it("statusCode checked 200", function (done) {
+    request(url, function (error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // checking status code json body parsed
+  it("body parsed statusCode checked 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // check result is a number
+  it("result returned is a number", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.be.a("number");
+      done();
+    });
+  });
+
+  // check result number is correct
+  it("result of subtracting 10, 20 is -10", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.equal(-10);
+      done();
+    });
+  });
+
+  // check result number is not incorrect
+  it("result is not -100", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.not.equal(-100);
+      done();
+    });
+  });
+});
+describe("Testing multiplication", function () {
+  var url = "http://localhost:8080/multiplyTwoNumbers/10/20";
+  // checking status code directly
+  it("statusCode checked 200", function (done) {
+    request(url, function (error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // checking status code json body parsed
+  it("body parsed statusCode checked 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // check result is a number
+  it("result returned is a number", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.be.a("number");
+      done();
+    });
+  });
+
+  // check result number is correct
+  it("result of multiply 10, 20 is 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.equal(200);
+      done();
+    });
+  });
+
+  // check result number is not incorrect
+  it("result is not -20", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.not.equal(-100);
+      done();
+    });
+  });
+});
+describe("Testing divide", function () {
+  var url = "http://localhost:8080/divideTwoNumbers/20/10";
+  // checking status code directly
+  it("statusCode checked 200", function (done) {
+    request(url, function (error, response) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // checking status code json body parsed
+  it("body parsed statusCode checked 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // check result is a number
+  it("result returned is a number", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.be.a("number");
+      done();
+    });
+  });
+
+  // check result number is correct
+  it("result of divide 10, 20 is 2", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.equal(2);
+      done();
+    });
+  });
+
+  // check result number is not incorrect
+  it("result is not -50", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.not.equal(-100);
+      done();
+    });
+  });
+});
+
